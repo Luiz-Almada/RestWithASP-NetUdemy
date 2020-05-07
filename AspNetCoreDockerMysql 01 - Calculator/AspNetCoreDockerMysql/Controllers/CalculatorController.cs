80ecb91f2d93837f8a6d3d1e37315174b6c7ef0a
Almada <altiva.sdk@gmail.com>
2020-05-06 21:18:48 -0300

    Implementing 2 - REST Calculator

--- AspNetCoreDockerMysql 01 - Calculator/AspNetCoreDockerMysql/Controllers/CalculatorController.cs
+++ AspNetCoreDockerMysql 01 - Calculator/AspNetCoreDockerMysql/Controllers/CalculatorController.cs
@@ -7,9 +7,9 @@ namespace AspNetCoreDockerMysql.Controllers
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
-        // GET api/values/5/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        // GET api/values/sum/5/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
             {
@@ -21,6 +21,99 @@ namespace AspNetCoreDockerMysql.Controllers
                 return Ok(sum.ToString());
             }
             
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/subtraction/5/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
+            {
+                return BadRequest("Numbers Invalid");
+            }
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
+            {
+                return BadRequest("Numbers Invalid");
+            }
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber) )
+            {
+                decimal numberSecondNumber;
+                decimal.TryParse(secondNumber, out numberSecondNumber);
+
+                if (numberSecondNumber == 0)
+                {
+                    return BadRequest("Invalid division by 0");
+                }
+
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
+            {
+                return BadRequest("Numbers Invalid");
+            }
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/average/5/5
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (String.IsNullOrEmpty(firstNumber) || String.IsNullOrEmpty(secondNumber))
+            {
+                return BadRequest("Numbers Invalid");
+            }
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var average = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(average.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/square-root/5
+        [HttpGet("square-root/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (String.IsNullOrEmpty(number))
+            {
+                return BadRequest("Number Invalid");
+            }
+            if (IsNumeric(number) )
+            {
+                var squareRoot = Math.Sqrt((double)ConvertToDecimal(number));
+                return Ok(squareRoot.ToString());
+            }
+
             return BadRequest("Invalid Input");
         }
 
